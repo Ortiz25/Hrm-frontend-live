be522d9f25a60ea276dde22d83095f7454ec0e42
Ortiz25 <samueldeya@gmail.com>
2024-09-26 16:50:39 +0300

    added warnings

--- src/pages/LeaveMgt.jsx
+++ src/pages/LeaveMgt.jsx
@@ -210,7 +210,7 @@ const LeaveManagementModule = () => {
             to="/calender"
             className=" border p-2  rounded shadow-lg hover:bg-slate-200"
           >
-            <Calendar className="inline mr-2" />
+            <Calendar className="inline mr-2 text-blue-400" />
             <span className="font-semibold text-lg ">Holidays</span>
           </NavLink>
 
